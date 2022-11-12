9dc0efda8e8faaa1b755e2a944b28242a40e97fc
Junior Moura <geraldomartins10@gmail.com>
2022-11-12 12:40:32 -0300

    definição do primeiro endpoint e do pacote model

---
+++ src/main/java/br/com/juniormoura/parking/controller/ParkingController.java
@@ -0,0 +1,24 @@
+package br.com.juniormoura.parking.controller;
+
+import br.com.juniormoura.parking.model.Parking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    @GetMapping
+    public List<Parking> findAll(){
+        var parking = new Parking();
+        parking.setColor("PRETO");
+        parking.setLicense("MSS-1111");
+        parking.setModel("VS GOL");
+        parking.setState("SP");
+        return Arrays.asList(parking);
+    }
+}

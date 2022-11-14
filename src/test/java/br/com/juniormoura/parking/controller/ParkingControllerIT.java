ae5b448546798600bd721805e61638b6ecf708fc
Junior Moura <geraldomartins10@gmail.com>
2022-11-14 11:42:22 -0300

    implementando os testes findAll() e create().

--- src/test/java/br/com/juniormoura/parking/controller/ParkingControllerIT.java
+++ src/test/java/br/com/juniormoura/parking/controller/ParkingControllerIT.java
@@ -1,10 +1,15 @@
 package br.com.juniormoura.parking.controller;
 
+import br.com.juniormoura.parking.controller.dto.ParkingCreateDto;
 import io.restassured.RestAssured;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkingControllerIT {
@@ -18,10 +23,32 @@ class ParkingControllerIT {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parking")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+
+
     }
 
     @Test
-    void create() {
+    void whenCreateThenCheckIsCreated() {
+        var createDTO = new ParkingCreateDto();
+        createDTO.setColor("AMARELA");
+        createDTO.setLicense("WTF-6666");
+        createDTO.setModel("BRASILIA");
+        createDTO.setState("CE");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parking")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license", Matchers.equalTo("WTF-6666"));
     }
 }
